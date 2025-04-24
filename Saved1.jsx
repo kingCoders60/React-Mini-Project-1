2d780f9fbbb29b473bf782798099d68ca2d9af15
kingCoders60 <spworld30@gmail.com>
2025-04-24 07:31:03 +0530

    done project 1

---
+++ Saved1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {  } from "module";
+
+const App = () => {
+  return (
+    <div>
+      <form>
+        <input className='px-4 py-3 text-xl mask-right rounded-2xl border-y-indigo-200' placeholder="Enter your name" />
+        <button className='px-4 py-3 text=xl font-semibold bg-amber-500 rounded'>Click-me</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
